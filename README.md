63f247c99425bda3bf30cf49896a806d6aebb35f
Kahvi Patel <iamkahvi@gmail.com>
2025-10-29 19:35:31 -0700

    adds nix develop and testing instructions

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Websocket server to use with markdown editor in browser [here](https://github.co
 ## Development
 `go run main.go` listens on port 8000
 
+`npx wscat -c ws://localhost:8000/write -H Origin:https://write.kahvipatel.com` then paste
+`{"type":"normal","status":"success","data":"this is kahvi"}`
+
 ## Routes
 `/` - homepage  
 `/write` - websocket
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Dev shell for go_markdown_server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in {
+      devShells = lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              golangci-lint
+            ];
+
+            shellHook = ''
+              export GOPATH="$PWD/.gopath"
+              export GOBIN="$GOPATH/bin"
+              export PATH="$GOBIN:$PATH"
+              mkdir -p "$GOBIN"
+            '';
+          };
+        });
+    };
+}
