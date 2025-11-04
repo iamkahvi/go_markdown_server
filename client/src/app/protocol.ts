e170c37eaf82ddcfdf63bbd72ee388f44d54dd41
Kahvi Patel <iamkahvi@gmail.com>
2025-11-04 00:41:08 -0800

    extracting

---
+++ client/src/app/protocol.ts
@@ -0,0 +1,21 @@
+export type Diff = [number, string];
+
+export type PatchObj = {
+  diffs: Diff[];
+  start1: number | null;
+  start2: number | null;
+  length1: number;
+  length2: number;
+};
+
+export type Patch = [-1 | 0 | 1, string];
+
+export interface Message {
+  patches: Patch[];
+  patchObjs: PatchObj[];
+}
+
+export interface MyResponse {
+  status: "OK" | "ERROR";
+  doc?: string;
+}
